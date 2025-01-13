5ce70a52e4c80639f3f7c245fcd9dffa23123e5e
Johannes Jira <Johannes.Jira@uni-bayreuth.de>
2025-01-13 19:38:52 +0100

    implement user service

--- business/src/main/java/de/unibayreuth/se/taskboard/business/impl/UserServiceImpl.java
+++ business/src/main/java/de/unibayreuth/se/taskboard/business/impl/UserServiceImpl.java
@@ -1,24 +1,34 @@
 package de.unibayreuth.se.taskboard.business.impl;
 
 import de.unibayreuth.se.taskboard.business.domain.User;
+import de.unibayreuth.se.taskboard.business.exceptions.UserNotFoundException;
+import de.unibayreuth.se.taskboard.business.ports.UserPersistenceService;
 import de.unibayreuth.se.taskboard.business.ports.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserPersistenceService userPersistenceService;
+
     @Override
     public User create(User user) {
-        return null;
+        return userPersistenceService.upsert(user);
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        return userPersistenceService.getAll();
     }
 
     @Override
-    public User getById(UUID id) {
-        return null;
+    public User getById(UUID id) throws UserNotFoundException {
+        if (userPersistenceService.getById(id).isPresent())
+            return userPersistenceService.getById(id).get();
+        throw new UserNotFoundException("User with ID " + id + "does not exist!");
     }
 }
--- business/src/main/java/de/unibayreuth/se/taskboard/business/ports/UserService.java
+++ business/src/main/java/de/unibayreuth/se/taskboard/business/ports/UserService.java
@@ -1,18 +1,17 @@
 package de.unibayreuth.se.taskboard.business.ports;
 
 import de.unibayreuth.se.taskboard.business.domain.User;
+import de.unibayreuth.se.taskboard.business.exceptions.UserNotFoundException;
 import org.springframework.lang.NonNull;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
-    //TODO: Add user service interface that the controller uses to interact with the business layer.
-    //TODO: Implement the user service interface in the business layer, using the existing user persistence service.
     @NonNull
     User create(@NonNull User user);
     @NonNull
     List<User> getAll();
     @NonNull
-    User getById(@NonNull UUID id);
+    User getById(@NonNull UUID id) throws UserNotFoundException;
 }
