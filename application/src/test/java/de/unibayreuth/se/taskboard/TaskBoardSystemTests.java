eb77c4428aaa1084e40a6371af2dc7f3fd05c749
Johannes Jira <Johannes.Jira@uni-bayreuth.de>
2025-01-14 19:20:06 +0100

    add tests

--- application/src/test/java/de/unibayreuth/se/taskboard/TaskBoardSystemTests.java
+++ application/src/test/java/de/unibayreuth/se/taskboard/TaskBoardSystemTests.java
@@ -3,22 +3,40 @@ package de.unibayreuth.se.taskboard;
 import de.unibayreuth.se.taskboard.api.dtos.TaskDto;
 import de.unibayreuth.se.taskboard.api.mapper.TaskDtoMapper;
 import de.unibayreuth.se.taskboard.business.domain.Task;
+import de.unibayreuth.se.taskboard.business.domain.User;
+import de.unibayreuth.se.taskboard.business.ports.UserService;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
+@AutoConfigureMockMvc(addFilters = false)
 public class TaskBoardSystemTests extends AbstractSystemTest {
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
     private TaskDtoMapper taskDtoMapper;
+
+    @MockBean
+    private UserService userService;
 
     @Test
     void getAllCreatedTasks() {
@@ -45,7 +63,7 @@ public class TaskBoardSystemTests extends AbstractSystemTest {
     @Test
     void createAndDeleteTask() {
         Task createdTask = taskService.create(
-                TestFixtures.getTasks().getFirst()
+                TestFixtures.getTasks().get(0)
         );
 
         when()
@@ -65,5 +83,27 @@ public class TaskBoardSystemTests extends AbstractSystemTest {
 
     }
 
-    //TODO: Add at least one test for each new endpoint in the users controller (the create endpoint can be tested as part of the other endpoints).
+    @Test
+    void getAllUsers() throws Exception {
+        List<User> expectedUsers = new ArrayList<>();
+        User user1 = new User("Alice");
+        expectedUsers.add(user1);
+        User user2 = new User("Bob");
+        expectedUsers.add(user2);
+
+        Mockito.when(userService.getAll()).thenReturn(expectedUsers);
+        mockMvc.perform(get("/api/users")).andExpect(status().isOk());
+        verify(userService, times(1)).getAll();
+    }
+
+    @Test
+    void getUserById() throws Exception {
+        User user = new User("Alice");
+
+        UUID uuid = UUID.fromString("7239cb7c-f443-4086-9669-ecb83cab8705");
+
+        Mockito.when(userService.getById(uuid)).thenReturn(user);
+        mockMvc.perform(get("/api/users/" + uuid.toString())).andExpect(status().isOk());
+        verify(userService, times(1)).getById(uuid);
+    }
 }
